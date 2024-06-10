6b3897f7d3b7fc7e1ee88c4e041c70d12e29a9d9
franklinhelio00 <franklinhelio00@gmail.com>
2024-06-10 18:48:29 -0300

    Add DTOs for authentication

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('Movie Catalog API')
+    .setDescription('API for movie catalog management')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = configService.get<string>('PORT') || 3000;
   await app.listen(port, '0.0.0.0');
 }
