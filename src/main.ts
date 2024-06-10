25f57debfe81c2200fa47c2c9a4860b7675690c1
franklinhelio00 <franklinhelio00@gmail.com>
2024-06-10 18:43:28 -0300

    Ensure application listens on the correct port

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle('Movie Catalog')
-    .setDescription('The Movie Catalog API description')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>('PORT') || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
