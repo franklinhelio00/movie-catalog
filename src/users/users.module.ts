a14476351efd5c4e400109a10d7ed5f142163ca5
franklinhelio00 <franklinhelio00@gmail.com>
2024-06-10 19:09:18 -0300

    Add Users module for authentication

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
+import { UsersRepository } from './users.repository';
 import { User } from './entities/user.entity';
 
 @Module({
